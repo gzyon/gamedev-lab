656dc1a994876dc188e44840e4d22f28ddbd21de
zhiyi <ongzhiyi@live.com>
2022-05-23 01:26:25 +0800

    basic lab 1 implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using TMPro;
 
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float maxSpeed = 10;
+    public float upSpeed = 10;
     private Rigidbody2D mario;
+    private SpriteRenderer marioSprite;
+    private bool faceRightState = true;
+    private bool onGroundState = true;
+    public Transform enemyLocation;
+    public TextMeshProUGUI scoreText;
+    private int score = 0;
+    private bool countScoreState = false;
  
     // Start is called before the first frame update
     void Start()
@@ -15,26 +25,73 @@ public class PlayerController : MonoBehaviour
         // Set to be 30 FPS
         Application.targetFrameRate =  30;
         mario = GetComponent<Rigidbody2D>();   
+        marioSprite = GetComponent<SpriteRenderer>();
     }
 
-    void  FixedUpdate()
+    void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
         // dynamic rigidbody
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        // mario move
         if (Mathf.Abs(moveHorizontal) > 0){
             Vector2 movement = new Vector2(moveHorizontal, 0);
             if (mario.velocity.magnitude < maxSpeed)    
                 mario.AddForce(movement * speed);
-            if (Input.GetKeyUp("a") || Input.GetKeyUp("d")){
-                // stop
-                mario.velocity = Vector2.zero;
-            }
         }
+
+        // mario stop
+        if (Input.GetKeyUp("a") || Input.GetKeyUp("d")){
+            mario.velocity = Vector2.zero;
+        }
+
+        // mario jump
+        if (Input.GetKeyDown("space") && onGroundState) {
+            // mario dont flip while jumping
+            mario.AddForce(Vector2.up * upSpeed, ForceMode2D.Impulse);
+            onGroundState = false;
+            countScoreState = true; //check if Goomba is underneath
+        }
+
+        // mario flip
+        if (Input.GetKeyDown("a") && faceRightState) {
+            faceRightState = false;
+            marioSprite.flipX = true;
+        }
+        if (Input.GetKeyDown("d") && !faceRightState) {
+            faceRightState = true;
+            marioSprite.flipX = false;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // when jumping, and Gomba is near Mario and we haven't registered our score
+        if (!onGroundState && countScoreState)
+        {
+            if (Mathf.Abs(transform.position.x - enemyLocation.position.x) < 0.5f)
+            {
+                countScoreState = false;
+                score++;
+                Debug.Log(score);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        Debug.Log("mario collided");
+        if (col.gameObject.CompareTag("Ground")) {
+            Debug.Log("Mario is on the floor");
+            onGroundState = true;
+            countScoreState = false; // reset score state
+            scoreText.text = "Score: " + score.ToString();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Enemy")) {
+            Debug.Log("Collided with Goomba!");
+        }
     }
 }
