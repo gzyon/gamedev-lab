3a12206ebeab010d959f0a7d4ceb4bdad723e196
felice <feliceho006@gmail.com>
2022-06-15 21:21:25 +0800

    merging with 4

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public  class GameManager : MonoBehaviour
+{
+	public  Text score;
+	private  int playerScore =  0;
+	
+	public  void  increaseScore(){
+		playerScore  +=  1;
+		score.text  =  "SCORE: "  +  playerScore.ToString();
+	}
+}
